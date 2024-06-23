4b0afca53dfd633f1ab6e0f7cc8a6148c72d8c4c
apalacios02 <allan.palacios28@gmail.com>
2024-06-23 17:26:23 -0500

    Added additional information to starships tab with links

--- app/controllers/starships_controller.rb
+++ app/controllers/starships_controller.rb
@@ -5,5 +5,7 @@ class StarshipsController < ApplicationController
 
   def show
     @starship = StarWarsApi.get_resource('starships', params[:id])
+    @pilots = (@starship['pilots'] || []).map { |url| StarWarsApi.get_resource_by_url(url) }
+    @films = (@starship['films'] || []).map { |url| StarWarsApi.get_resource_by_url(url) }
   end
 end
