c03c8f70083494735cccbaad695f2857f110a664
apalacios02 <allan.palacios28@gmail.com>
2024-06-23 17:38:21 -0500

    Added additional information to planets tab with links

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -5,5 +5,7 @@ class PlanetsController < ApplicationController
 
   def show
     @planet = StarWarsApi.get_resource('planets', params[:id])
+    @residents = (@planet['residents'] || []).map { |url| StarWarsApi.get_resource_by_url(url) }
+    @films = (@planet['films'] || []).map { |url| StarWarsApi.get_resource_by_url(url) }
   end
 end
