06c6f55897ce95e99ef6f34cafaed1cb3a2ee8db
apalacios02 <allan.palacios28@gmail.com>
2024-06-23 17:17:58 -0500

    Added additional information to film tab with links

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -5,5 +5,9 @@ class FilmsController < ApplicationController
 
   def show
     @film = StarWarsApi.get_resource('films', params[:id])
+    @people = (@film['characters'] || []).map { |url| StarWarsApi.get_resource_by_url(url) }
+    @vehicles = (@film['vehicles'] || []).map { |url| StarWarsApi.get_resource_by_url(url) }
+    @starships = (@film['starships'] || []).map { |url| StarWarsApi.get_resource_by_url(url) }
+    @planets = (@film['planets'] || []).map { |url| StarWarsApi.get_resource_by_url(url) }
   end
 end
