708b3fc914c0a8c43c19d43c375ebdab0bebb371
apalacios02 <allan.palacios28@gmail.com>
2024-06-25 08:02:05 -0500

    added functionality to identify if person is jedi or not

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,6 +4,8 @@ class PeopleController < ApplicationController
   def index
     if params[:query].present?
       @people = search_people(params[:query])
+    elsif params[:type] == 'jedi'
+      @people = fetch_jedi_people
     else
       page = params[:page].to_i.positive? ? params[:page].to_i : 1
       @people = fetch_all_people(page)
@@ -11,6 +13,11 @@ class PeopleController < ApplicationController
 
     @total_pages = total_pages
     @current_page = params[:page].to_i.positive? ? params[:page].to_i : 1
+  rescue Timeout::Error => e
+    flash.now[:alert] = "Request timed out: #{e.message}"
+    @people = []
+    @total_pages = 1
+    @current_page = 1
   end
 
   def show
@@ -18,6 +25,10 @@ class PeopleController < ApplicationController
     @vehicles = @person['vehicles'].map { |url| StarWarsApi.get_resource_by_url(url) }
     @starships = @person['starships'].map { |url| StarWarsApi.get_resource_by_url(url) }
     @homeworld = StarWarsApi.get_resource_by_url(@person['homeworld'])
+    @is_jedi = jedi_names.include?(@person['name'])
+  rescue Timeout::Error => e
+    flash.now[:alert] = "Request timed out: #{e.message}"
+    redirect_to root_path
   end
 
   private
@@ -25,12 +36,14 @@ class PeopleController < ApplicationController
   def fetch_all_people(page)
     people = []
     url = "https://swapi.dev/api/people/?page=#{page}"
-    response = HTTParty.get(url)
-    if response.success?
-      data = response.parsed_response
-      people = data['results']
-    else
-      flash.now[:alert] = 'Failed to fetch people from Star Wars API'
+    Timeout.timeout(10) do
+      response = HTTParty.get(url)
+      if response.success?
+        data = response.parsed_response
+        people = data['results']
+      else
+        flash.now[:alert] = 'Failed to fetch people from Star Wars API'
+      end
     end
     people
   end
@@ -64,5 +77,10 @@ class PeopleController < ApplicationController
 
   def set_person
     @person = StarWarsApi.get_resource('people', params[:id])
+  end
+
+  def jedi_names
+    # List of known Jedi names. This list can be extended or modified based on the dataset or API
+    ["Luke Skywalker", "Obi-Wan Kenobi", "Yoda", "Mace Windu", "Anakin Skywalker", "Rey", "Qui-Gon Jinn"]
   end
 end
