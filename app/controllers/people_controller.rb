312a334293bdc34f1ef9c51c1104643294e47779
apalacios02 <allan.palacios28@gmail.com>
2024-06-25 08:09:23 -0500

    added tabs for all people and for jedi list

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -13,11 +13,6 @@ class PeopleController < ApplicationController
 
     @total_pages = total_pages
     @current_page = params[:page].to_i.positive? ? params[:page].to_i : 1
-  rescue Timeout::Error => e
-    flash.now[:alert] = "Request timed out: #{e.message}"
-    @people = []
-    @total_pages = 1
-    @current_page = 1
   end
 
   def show
@@ -26,9 +21,6 @@ class PeopleController < ApplicationController
     @starships = @person['starships'].map { |url| StarWarsApi.get_resource_by_url(url) }
     @homeworld = StarWarsApi.get_resource_by_url(@person['homeworld'])
     @is_jedi = jedi_names.include?(@person['name'])
-  rescue Timeout::Error => e
-    flash.now[:alert] = "Request timed out: #{e.message}"
-    redirect_to root_path
   end
 
   private
@@ -36,14 +28,12 @@ class PeopleController < ApplicationController
   def fetch_all_people(page)
     people = []
     url = "https://swapi.dev/api/people/?page=#{page}"
-    Timeout.timeout(10) do
-      response = HTTParty.get(url)
-      if response.success?
-        data = response.parsed_response
-        people = data['results']
-      else
-        flash.now[:alert] = 'Failed to fetch people from Star Wars API'
-      end
+    response = HTTParty.get(url)
+    if response.success?
+      data = response.parsed_response
+      people = data['results']
+    else
+      flash.now[:alert] = 'Failed to fetch people from Star Wars API'
     end
     people
   end
@@ -75,12 +65,28 @@ class PeopleController < ApplicationController
     people
   end
 
-  def set_person
-    @person = StarWarsApi.get_resource('people', params[:id])
+  def fetch_jedi_people
+    people = []
+    jedi_list = jedi_names
+    jedi_list.each do |jedi|
+      url = "https://swapi.dev/api/people/?search=#{jedi}"
+      response = HTTParty.get(url)
+      if response.success?
+        data = response.parsed_response
+        people.concat(data['results'])
+      else
+        flash.now[:alert] = 'Failed to fetch Jedi from Star Wars API'
+      end
+    end
+    people
   end
 
   def jedi_names
     # List of known Jedi names. This list can be extended or modified based on the dataset or API
     ["Luke Skywalker", "Obi-Wan Kenobi", "Yoda", "Mace Windu", "Anakin Skywalker", "Rey", "Qui-Gon Jinn"]
+  end
+
+  def set_person
+    @person = StarWarsApi.get_resource('people', params[:id])
   end
 end
